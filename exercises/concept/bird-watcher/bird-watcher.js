e8215f1279ea492e3f027c255aabf1ed2b3e7993
Chandan8585 <chandanwingshr@gmail.com>
2023-03-13 12:47:34 +0530

    Bird Watcher Completed

--- exercises/concept/bird-watcher/bird-watcher.js
+++ exercises/concept/bird-watcher/bird-watcher.js
@@ -3,7 +3,6 @@
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
-
 /**
  * Calculates the total bird count.
  *
@@ -11,9 +10,8 @@
  * @returns {number} total bird count
  */
 export function totalBirdCount(birdsPerDay) {
-  throw new Error('Please implement the totalBirdCount function');
+  return birdsPerDay.reduce((total, current) => total + current);
 }
-
 /**
  * Calculates the total number of birds seen in a specific week.
  *
@@ -22,9 +20,9 @@ export function totalBirdCount(birdsPerDay) {
  * @returns {number} birds counted in the given week
  */
 export function birdsInWeek(birdsPerDay, week) {
-  throw new Error('Please implement the birdsInWeek function');
+  let oneWeek = birdsPerDay.splice((week - 1) * 7, 7);
+  return totalBirdCount(oneWeek);
 }
-
 /**
  * Fixes the counting mistake by increasing the bird count
  * by one for every second day.
@@ -33,5 +31,8 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {number[]} corrected bird count data
  */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Please implement the fixBirdCountLog function');
+  for (let day = 0; day < birdsPerDay.length; day += 2) {
+    birdsPerDay[day]++;
+  }
+  return birdsPerDay;
 }
